f0334432612f90ec192c30f2114425a42edb3b28
Jeroen Wienk <jeroenwienk@gmail.com>
2017-06-11 18:46:28 +0200

    print out the duration

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+let mainWindow = null;
 
 app.on('ready', () => {
   console.log('App is ready');
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('fileSubmit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    if (err) {
+      console.log(err)
+    } else {
+      mainWindow.webContents.send('fileMetadata', metadata.format.duration);
+    }
+  });
 });
